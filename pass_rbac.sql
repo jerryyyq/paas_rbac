37b76c5bcec06c958578d8b91c0bc7bcb92361ef
yangyuqi@sina.com <yangyuqi@sina.com>
2017-08-30 00:14:50 +0800

    add table

--- pass_rbac.sql
+++ pass_rbac.sql
@@ -43,6 +43,7 @@ CREATE TABLE `ac_resource` (
   `id_resource` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) DEFAULT NULL,
   `show_name` varchar(128) DEFAULT NULL,
+  `type` int(11) NOT NULL COMMENT '0:sys, 1:enteprise, 2:websit',
   PRIMARY KEY (`id_resource`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -59,6 +60,7 @@ CREATE TABLE `ac_rule` (
   `name` varchar(128) DEFAULT NULL,
   `show_name` varchar(128) DEFAULT NULL,
   `description` varchar(256) DEFAULT NULL,
+  `type` int(11) NOT NULL COMMENT '0:sys, 1:enteprise, 2:websit',
   PRIMARY KEY (`id_rule`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -140,6 +142,29 @@ CREATE TABLE `sys_admin` (
   PRIMARY KEY (`idadmin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `website`
+--
+
+DROP TABLE IF EXISTS `website`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `website` (
+  `id_website` int(11) NOT NULL AUTO_INCREMENT,
+  `id_enterprise` int(11) NOT NULL COMMENT '属于哪个企业的',
+  `name` varchar(512) DEFAULT NULL,
+  `url` varchar(128) NOT NULL,
+  `symbol_name` varchar(128) DEFAULT NULL COMMENT 'url 中 子域名',
+  `style` int(11) NOT NULL DEFAULT '0' COMMENT '网站风格（皮肤和式样）',
+  `description` varchar(256) DEFAULT NULL,
+  `state` int(11) DEFAULT NULL,
+  `registe_date` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_website`),
+  KEY `fk_website_1_idx` (`id_enterprise`),
+  CONSTRAINT `fk_website_1` FOREIGN KEY (`id_enterprise`) REFERENCES `enterprise` (`id_enterprise`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -150,4 +175,4 @@ CREATE TABLE `sys_admin` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-27 21:05:18
+-- Dump completed on 2017-08-30  0:11:21
